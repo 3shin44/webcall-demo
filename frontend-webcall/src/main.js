7b7b237efc7539ed3e9e9ddf8aca5b2227dd5f9c
Rico Huang <RicoHuang@crm.com.tw>
2023-02-06 14:29:54 +0800

    import packages

--- frontend-webcall/src/main.js
+++ frontend-webcall/src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
+// import customerize scss
+import "../src/assets/css/index.scss"
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
---
+++ frontend-webcall/src/views/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h2>HELLO</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
