3c9c2e141a2617714919bfa62e7dac6cabcfc771
Rico Huang <RicoHuang@crm.com.tw>
2023-02-17 12:54:21 +0800

    update theme materials

--- frontend-webcall/vue.config.js
+++ frontend-webcall/vue.config.js
@@ -3,4 +3,12 @@ module.exports = defineConfig({
   // 關閉雜湊檔名, 預設路徑為專案相對路徑
   publicPath:'./',
   filenameHashing: false,
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: '新光金控Web Call'
+    }
+  }
 })
